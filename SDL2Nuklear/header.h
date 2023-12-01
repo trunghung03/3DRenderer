2bb7b7d02e3e435c716911133d87e8dfaf3e820c
synapse26 <61304518+trunghung03@users.noreply.github.com>
2023-12-01 22:22:04 +0700

    Add project files.

---
+++ SDL2Nuklear/header.h
@@ -0,0 +1,27 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
+#include <SDL2/SDL.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_SDL_RENDERER_IMPLEMENTATION
+#pragma warning(push, 0)
+#include "nuklear.h"
+#include "nuklear_sdl_renderer.h"
+#pragma warning(pop)
---
+++ SDL2Nuklear/main.c
@@ -0,0 +1,165 @@
+#include "header.h"
+
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+void setupSDL();
+void setupNuklear();
+
+// SDL
+SDL_Window* win;
+SDL_Renderer* renderer;
+// Nuklear
+struct nk_context* ctx;
+
+void setupSDL() {
+    /* SDL setup */
+    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, "0");
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    win = SDL_CreateWindow("Demo",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (win == NULL) {
+        SDL_Log("Error SDL_CreateWindow %s", SDL_GetError());
+        exit(-1);
+    }
+
+    int render_flags = 0;
+    render_flags |= SDL_RENDERER_ACCELERATED;
+    render_flags |= SDL_RENDERER_PRESENTVSYNC;
+
+    renderer = SDL_CreateRenderer(win, -1, render_flags);
+
+    if (renderer == NULL) {
+        SDL_Log("Error SDL_CreateRenderer %s", SDL_GetError());
+        exit(-1);
+    }
+
+    /* scale the renderer output for High-DPI displays */
+    {
+        int render_w, render_h;
+        int window_w, window_h;
+        float scale_x, scale_y;
+        SDL_GetRendererOutputSize(renderer, &render_w, &render_h);
+        SDL_GetWindowSize(win, &window_w, &window_h);
+        scale_x = (float)(render_w) / (float)(window_w);
+        scale_y = (float)(render_h) / (float)(window_h);
+        SDL_RenderSetScale(renderer, scale_x, scale_y);
+    }
+}
+
+void setupNuklear() {
+    /* GUI */
+    ctx = nk_sdl_init(win, renderer);
+    {
+        struct nk_font_atlas* atlas;
+        struct nk_font_config config = nk_font_config(0);
+        struct nk_font* font;
+
+        /* set up the font atlas and add desired font; note that font sizes are
+         * multiplied by font_scale to produce better results at higher DPIs */
+        nk_sdl_font_stash_begin(&atlas);
+        font = nk_font_atlas_add_default(atlas, 13, &config);
+        nk_sdl_font_stash_end();
+
+        nk_style_set_font(ctx, &font->handle);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    setupSDL();
+    setupNuklear();
+
+    int running = 1;
+    /* GUI */
+    struct nk_colorf bg = { 0, 0, 0, 0 };
+    bg.r = 0.10f, bg.g = 0.18f, bg.b = 0.24f, bg.a = 1.0f;
+
+    while (running)
+    {
+        /* Input */
+        SDL_Event evt;
+        nk_input_begin(ctx);
+        while (SDL_PollEvent(&evt)) {
+            if (evt.type == SDL_QUIT) goto cleanup;
+            nk_sdl_handle_event(&evt);
+        }
+        nk_input_end(ctx);
+
+        enum { EASY, HARD };
+        static int op = EASY;
+        static float value = 0.6f;
+
+        /* GUI */
+        if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
+            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            
+            static int property = 20;
+
+            nk_layout_row_static(ctx, 30, 80, 1);
+            if (nk_button_label(ctx, "button"))
+                fprintf(stdout, "button pressed\n");
+            nk_layout_row_dynamic(ctx, 30, 2);
+            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+            nk_layout_row_dynamic(ctx, 25, 1);
+            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+            nk_layout_row_dynamic(ctx, 20, 1);
+            nk_label(ctx, "background:", NK_TEXT_LEFT);
+            nk_layout_row_dynamic(ctx, 25, 1);
+            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
+                nk_layout_row_dynamic(ctx, 120, 1);
+                bg = nk_color_picker(ctx, bg, NK_RGBA);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
+                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
+                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
+                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
+                nk_combo_end(ctx);
+            }
+        }
+        nk_end(ctx);
+
+        if (nk_begin(ctx, "Show", nk_rect(500, 50, 220, 220),
+            NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_CLOSABLE)) {
+            // fixed widget pixel width
+            nk_layout_row_static(ctx, 30, 80, 1);
+            if (nk_button_label(ctx, "button")) {
+                // event handling
+            }
+            // fixed widget window ratio width
+            nk_layout_row_dynamic(ctx, 30, 2);
+            if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
+            if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
+            // custom widget pixel width
+            nk_layout_row_begin(ctx, NK_STATIC, 30, 2);
+            {
+                nk_layout_row_push(ctx, 50);
+                nk_label(ctx, "Volume:", NK_TEXT_LEFT);
+                nk_layout_row_push(ctx, 110);
+                nk_slider_float(ctx, 0, &value, 1.0f, 0.1f);
+            }
+            nk_layout_row_end(ctx);
+        }
+        nk_end(ctx);
+
+        SDL_SetRenderDrawColor(renderer, (Uint8) (bg.r * 255), (Uint8) (bg.g * 255), (Uint8) (bg.b * 255), (Uint8) (bg.a * 255));
+        SDL_RenderClear(renderer);
+
+        nk_sdl_render(NK_ANTI_ALIASING_ON);
+
+        SDL_RenderPresent(renderer);
+    }
+
+cleanup:
+    nk_sdl_shutdown();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
